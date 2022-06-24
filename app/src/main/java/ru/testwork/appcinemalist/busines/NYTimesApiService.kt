59a70d7e83818fe7c58124f266433497bbdec0b2
toffeantyri <tofy3434@yandex.ru>
2022-06-24 11:24:44 +0600

    NyTimes service add query offset with dafault value : "0"(startPage 0)

--- app/src/main/java/ru/testwork/appcinemalist/busines/NYTimesApiService.kt
+++ app/src/main/java/ru/testwork/appcinemalist/busines/NYTimesApiService.kt
@@ -12,11 +12,17 @@ import ru.testwork.appcinemalist.busines.model.NYTimesReviewModel
 interface NYTimesApiService {
 
 
-    // http://api.nytimes.com/svc/movies/v2/reviews/all.json?api-key=WLZea1BzGd1A7R9duap3nIJsAfi4JfSo
+    // http://api.nytimes.com/svc/movies/v2/reviews/all.json?offset=21&api-key=WLZea1BzGd1A7R9duap3nIJsAfi4JfSo
+
+    //call с 0 по 20 вкл Next Call с 20 что бы без повторений
+
+    private val startPage: String
+        get() = "0"
 
     @GET("/svc/movies/v2/reviews/all.json?")
     fun getReviewAll(
-        @Query("api-key") key : String = APP_CONTEXT.getString(R.string.api_key),
+        @Query("offset") offset : String = startPage,
+        @Query("api-key") key : String = APP_CONTEXT.getString(R.string.api_key)
     ) : Response<NYTimesReviewModel>
 
 }
